fbd71cf65f4eb87b61c070b11c7d379d9674b6f7
7korobi <7korobi@BA-1810001-MAC.local>
2019-02-28 18:22:40 +0900

    Hello World!

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,18 @@
 module.exports = {
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    "^@/(.*)$": "<rootDir>/$1",
+    "^~/(.*)$": "<rootDir>/$1",
+    "^vue$": "vue/dist/vue.common.js"
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  globals: {
+    "ts-jest": {
+      diagnostics: false
+    }
+  },
+  moduleFileExtensions: ["ts", "js", "vue", "json"],
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
+    ".*\\.(vue)$": "vue-jest"
   }
-}
+};

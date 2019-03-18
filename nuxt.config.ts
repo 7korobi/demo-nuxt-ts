d05c1d627c90cd9c7ba1a92a098ca3623b9cd541
7korobi <7korobi@BA-1810001-MAC.local>
2019-03-18 20:22:31 +0900

    web app manifest.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,12 +12,22 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'msapplication-TileColor', content: "#ffc40d" },
+      { name: 'theme-color', content: "#ffffff" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/images/icon/favicon.ico' },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/images/icon/apple-touch-icon.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/images/icon/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/images/icon/favicon-16x16.png" },
+      { rel: "mask-icon", href: "/images/icon/safari-pinned-tab.svg", color: "#5bbad5" },
+      { rel: "manifest", href: "/manifest.json" },
     ]
   },
+
+
+
 
   /*
   ** Customize the progress-bar color
@@ -72,5 +82,6 @@ module.exports = {
   manifest: {
     name: 'Nuxt TS demo',
     lang: 'ja',
+    nativeUI: true
   }
 }
